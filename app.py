9914f89242e658d05e43fd862f04839759ff0988
NlanubTech <nlanubtech@gmail.com>
2023-03-17 13:39:50 -0500

    10

--- app.py
+++ app.py
@@ -33,59 +33,59 @@ def articulo1():
 
 @app.route('/articulo2')
 def articulo2():
-    return render_template('articulo2')
+    return render_template('articulo2.html')
 
 @app.route('/articulo3')
 def articulo3():
-    return render_template( 'articulo3')
+    return render_template( 'articulo3.html')
 
 @app.route('/articulo4')
 def articulo4():
-    return render_template( 'articulo4')
+    return render_template( 'articulo4.html')
 
 @app.route('/articulo5')
 def articulo5():
-    return render_template( 'articulo5')
+    return render_template( 'articulo5.html')
 
 @app.route('/articulo6')
 def marticulo6():
-    return render_template( 'articulo6')
+    return render_template( 'articulo6.html')
 
 @app.route('/articulo7')
 def articulo7():
-    return render_template( 'articulo7')
+    return render_template( 'articulo7.html')
 
 @app.route('/articulo8')
 def articulo8():
-    return render_template( 'articulo8')
+    return render_template( 'articulo8.html')
 
 @app.route('/articulo9')
 def articulo9():
-    return render_template( 'articulo9')
+    return render_template( 'articulo9.html')
 
 @app.route('/articulo10')
 def articulo10():
-    return render_template( 'articulo10')
+    return render_template( 'articulo10.html')
 
 @app.route('/articulo11')
 def articulo11():
-    return render_template( 'articulo11')
+    return render_template( 'articulo11.html')
 
 @app.route('/articulo12')
 def articulo12():
-    return render_template( 'articulo12')
+    return render_template( 'articulo12.html')
 
 @app.route('/articulo13')
 def articulo13():
-    return render_template( 'articulo13')
+    return render_template( 'articulo13.html')
 
 @app.route('/articulo14')
 def articulo14():
-    return render_template( 'articulo14')
+    return render_template( 'articulo14.html')
 
 @app.route('/articulo15')
 def articulo15():
-    return render_template( 'articulo15')
+    return render_template( 'articulo15.html')
 
 
 if __name__ == '__main__':
